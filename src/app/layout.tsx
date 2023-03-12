0370b85a423c380c1ea050c7177b864f1df52efa
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2023-03-12 12:02:56 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <head />
+      <body>
+        <CacheProvider>
+          <ChakraProvider>{children}</ChakraProvider>
+        </CacheProvider>
+      </body>
+    </html>
+  );
+}
