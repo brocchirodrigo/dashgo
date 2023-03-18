8fde63b788200ae616d1484d2fb85eed986e8104
Rodrigo Brocchi <rodrigo.brocchi@gmail.com>
2023-03-18 16:47:46 -0300

    Sidebar responsivo

---
+++ src/components/Sidebar/SidebarNav.tsx
@@ -0,0 +1,32 @@
+import { Stack } from "@chakra-ui/react";
+
+import {
+  RiContactsBookLine,
+  RiDashboardLine,
+  RiGitMergeLine,
+  RiInputMethodLine,
+} from "react-icons/ri";
+
+import { NavLink } from "./NavLink";
+import { NavSection } from "./NavSection";
+export function SidebarNav() {
+  return (
+    <Stack spacing="12" align="flex-start">
+      <NavSection title="GERAL">
+        <NavLink href="/dashboard" icon={RiDashboardLine}>
+          Dashboard
+        </NavLink>
+
+        <NavLink href="/users" icon={RiContactsBookLine}>
+          Usuários
+        </NavLink>
+      </NavSection>
+
+      <NavSection title="AUTOMAÇÃO">
+        <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
+
+        <NavLink icon={RiGitMergeLine}>Automação</NavLink>
+      </NavSection>
+    </Stack>
+  );
+}
